ff79c07319dd65b4beacfa14f32b1d28b4b55815
Arthur-Soares <asoaresdasilva@outlook.com>
2022-01-26 20:05:13 -0300

    Inclusões cursos de Dominando IDEs e Variaveis, Tipos de Dados JAVA

---
+++ Variáveis, Tipos de Dados e Operadores Matemáticos em Java/exercitando-1/src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,53 @@
+package one.digitalinnovation.basecamp;
+
+/**
+* Classe de exemplo para o exercicio da Aula 1 de Variaveis, Tipos de Dados e Operadores aritméticos.
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i;
+        //int i;
+        int I;
+        //int 1a;
+        int _a1;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _a1 = 20;
+        $aq =7;
+
+        final int j = 10;
+        //j = 15;
+        int asrn24678md;
+        //int asrn246 78md;
+        int asrn$246_78md = 10;
+        //int asrn2$46%78_md = 10;
+
+        asrn24678md = 100;
+        asrn$246_78md = 10;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_a1);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        System.out.println(asrn$246_78md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+    }
+}
